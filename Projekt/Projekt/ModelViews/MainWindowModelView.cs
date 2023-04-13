03e1873c05ec539f86b236e3ff8521b0d9bcdfc4
sosso <cypossosmu51c@gmail.com>
2023-04-13 15:13:08 +0200

    App was made

--- Projekt/Projekt/ModelViews/MainWindowModelView.cs
+++ Projekt/Projekt/ModelViews/MainWindowModelView.cs
@@ -21,8 +21,8 @@ namespace Projekt.ModelViews
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        private string arg1;
-        public string Arg1
+        private int arg1;
+        public int Arg1
         {
             get { return arg1; }
             set
@@ -31,7 +31,8 @@ namespace Projekt.ModelViews
                 OnPropertyChanged(nameof(Arg1));
             }
         }
-        public string Arg2
+        private int arg2;
+        public int Arg2
         {
             get { return arg2; }
             set
@@ -40,22 +41,36 @@ namespace Projekt.ModelViews
                 OnPropertyChanged(nameof(Arg2));
             }
         }
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                header = value;
+                OnPropertyChanged(nameof(Header));
+            }
+        }
         public ICommand NWDCommand { get; set; }
         public void NWD(Object obj)
         {
             try
             {
-                while(Arg1 != Arg2)
+                int a = Convert.ToInt32(Arg1);
+                int b = Convert.ToInt32(Arg2);
+                while(a != b)
                 {
-                    if(Arg1 > Arg2)
+                    if(a > b)
                     {
-                        Arg1 = Arg1 - Arg2;
+                        a -= b;
                     }
-                    if(obj > Arg1)
+                    if(b > a)
                     {
-                        Arg2 = Arg2 - Arg1;
+                        b -= a;
                     }
                 }
+
+                Header = a.ToString();
             }
             catch (Exception ex)
             {
